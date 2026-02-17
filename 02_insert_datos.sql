3ada6d6a6d89cdbd2ab6dab72eb708022466d4cf
jhonmel15 <jhommelberroaramirez@gmail.com>
2026-02-16 23:04:00 -0400

    Create 02_insert_datos.sql

---
+++ 02_insert_datos.sql
@@ -0,0 +1,7 @@
+INSERT INTO usuarios(nombre, apellido, email) VALUES
+('Juan', 'Perez', 'juan@email.com'),
+('Maria', 'Lopez', 'maria@email.com');
+
+INSERT INTO cuenta(usuario_id, monto) VALUES
+(1, 1000.00),
+(2, 500.00);
